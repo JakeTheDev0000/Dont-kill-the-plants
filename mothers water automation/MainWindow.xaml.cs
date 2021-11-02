43624dfa9e69bcc103684cdd98e1b1f75aff69b0
Jacob Obrien <jacobrien64@gmail.com>
2021-11-01 23:25:49 -0700

    added the 'message box' to inform you to do things

--- mothers water automation/MainWindow.xaml.cs
+++ mothers water automation/MainWindow.xaml.cs
@@ -33,7 +33,12 @@ namespace mothers_water_automation
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             string daysName = System.DateTime.Now.ToString("dddd");
-            
+            bool needsToWaterAtA = false;
+            bool needsToWaterAtB= false;
+            bool needsToWaterAtC = false;
+
+            string monthName = DateTime.Now.ToString("MMMM");
+
             if (daysJakesName.Text == "")
             {
 
@@ -45,12 +50,40 @@ namespace mothers_water_automation
 
             if (daysName == "Monday")
             {
-                MessageBox.Show("mon");
+                needsToWaterAtA = true;
             }
             else if (daysName == "Tuesday")
             {
-                MessageBox.Show("tue");
+                needsToWaterAtB = true;
             }
+            else if (daysName == "Wednesday")
+            {
+                needsToWaterAtC = true;
+            }
+            else if (daysName == "Thursday")
+            {
+                needsToWaterAtA = true;
+                needsToWaterAtB = true;
+            }
+            else if (daysName == "Friday")
+            {
+                needsToWaterAtC = true;
+            }
+            else if (daysName == "Saturday")
+            {
+                needsToWaterAtB = true;
+            }
+            else if (daysName == "Sunday")
+            {
+                needsToWaterAtA = true;
+                needsToWaterAtB = true;
+                needsToWaterAtC = true;
+            }
+
+            MessageBox.Show($"its {monthName}\nIts also {daysName} so that means you need to water at section [sectionNameForToday]");
+
+
+
         }
     }
 }
